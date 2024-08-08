8b89e8b840a5b4b7e8e6e66d50f11dd8be5dea10
Bhaswar004 <167642120+Bhaswar004@users.noreply.github.com>
2024-08-08 23:05:18 +0530

    home page

--- app/src/main/java/eu/tutorials/loginandsignupwithfirebase/MainActivity.kt
+++ app/src/main/java/eu/tutorials/loginandsignupwithfirebase/MainActivity.kt
@@ -1,18 +1,30 @@
 package eu.tutorials.loginandsignupwithfirebase
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
+import com.denzcoskun.imageslider.ImageSlider
+import com.denzcoskun.imageslider.constants.ScaleTypes
+import com.denzcoskun.imageslider.models.SlideModel
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         var NavController = findNavController(R.id.fragmentContainerView)
         var bottomnav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
         bottomnav.setupWithNavController(NavController)
+
+
+
+
+
 
     }
 }
